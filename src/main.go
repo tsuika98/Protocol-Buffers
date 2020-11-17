ff766ebc3978e0ef5466284b64b07e10ba66bad1
tsuika98 <tsuika98@gmail.com>
2020-11-16 22:26:23 -0600

    clean up example

--- src/main.go
+++ src/main.go
@@ -13,11 +13,33 @@ func main() {
 		Age:  22,
 	}
 
+	log.Print("Person before marshal")
+	printPersonInfo(p)
+
 	// marshall it into protobuff format
 	data, err := proto.Marshal(p)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Print("Protobuf data: ", data)
+	log.Print("Person after marshalling: ", data)
+
+	// unmarshal protocol buffer object into a person type
+	newP := &Person{}
+	err = proto.Unmarshal(data, newP)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Print("Person after unmarshalling: ", newP)
+	printPersonInfo(p)
+
+}
+
+func printPersonInfo(person *Person) {
+	// use generated methods in person.pb file to retrieve field names of the person type
+	log.Print("Name: ", person.GetName())
+	log.Print("Age: ", person.GetAge())
+	log.Print("\n")
+
 }
